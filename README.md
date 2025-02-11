ba6f0045facd9566cce28a17786a368b66af272c
no name <Neemiaslauriano8@gmail.com>
2025-02-11 01:18:08 +0000

    adiciona scritps `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 - 👋 Hi, I’m @gold1dream
 - 👀 I’m interested in programming
 - 🌱 I’m currently learning programming
+
+```python
 - ⚡ Fun fact: i study programming
+```
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
-function Home() {return <h1> "não existe vento favorável para o marinheiro que não sabe a onde ir". -SENECA
-</h1>}
+function Home() {
+  return (
+    <h1>
+      {" "}
+      "não existe vento favorável para o marinheiro que não sabe a onde ir".
+      -SENECA
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("indentação errada");
+}
 
 export default Home;
