6ae555e7cb40bbd47b981e014fefc51371b4a0b4
Gold.WRLD <Neemiaslauriano8@gmail.com>
2026-01-29 20:48:41 +0000

    problemas com o watch

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "postgres",
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+}
+export default {
+  query: query,
+};
